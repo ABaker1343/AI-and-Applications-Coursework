148aa37a6509bee4f32d143ad3e3e229b4566de3
Alex <billybo1234@outlook.com>
2022-03-14 12:19:43 +0000

    allowed for a default board

--- SudokuSolver/SolverApp.java
+++ SudokuSolver/SolverApp.java
@@ -7,45 +7,51 @@ public class SolverApp {
     
     public static void main(String args[]){
 
-        String filepath = args[0];
-        ArrayList<Character> charBuffer = new ArrayList<Character>();
-
-        try (FileReader fr = new FileReader(filepath)){
-            int newChar = -1;
-            while((newChar = fr.read()) != -1){
-                if ( Character.isDigit( (char)newChar ) || (char)newChar == '.'){
-                    charBuffer.add((char)newChar);
-                }
-            }
-        }
-        catch(Exception e){
-            System.out.println(e.getMessage());
-        }
-
+        
         int[][] startBoard = new int[9][9];
 
-        for (int r = 0; r < 9; r++){
-            for (int c = 0; c < 9; c++){
-                if (charBuffer.get(r+c) == '.'){
-                    startBoard[r][c] = 0;
+        if (args.length > 0){
+            String filepath = args[0];
+            ArrayList<Character> charBuffer = new ArrayList<Character>();
+
+            try (FileReader fr = new FileReader(filepath)){
+                int newChar = -1;
+                while((newChar = fr.read()) != -1){
+                    if ( Character.isDigit( (char)newChar ) || (char)newChar == '.'){
+                        charBuffer.add((char)newChar);
+                    }
                 }
-                else {
-                    startBoard[r][c] = charBuffer.get(r+c) - '0'; // - character 0 to get numerical value
+            }
+            catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+    
+
+            for (int r = 0; r < 9; r++){
+                for (int c = 0; c < 9; c++){
+                    if (charBuffer.get(r+c) == '.'){
+                        startBoard[r][c] = 0;
+                    }
+                    else {
+                        startBoard[r][c] = charBuffer.get(r+c) - '0'; // - character 0 to get numerical value
+                    }
                 }
             }
         }
 
-        // startBoard = {
-        //     {5,3,0,0,7,0,0,0,0},
-        //     {6,0,0,1,9,5,0,0,0},
-        //     {0,9,8,0,0,0,0,6,0},
-        //     {8,0,0,0,6,0,0,0,3},
-        //     {4,0,0,8,0,3,0,0,1},
-        //     {7,0,0,0,2,0,0,0,6},
-        //     {0,6,0,0,0,0,2,8,0},
-        //     {0,0,0,4,1,9,0,0,5},
-        //     {0,0,0,0,8,0,0,7,9}
-        // };
+        else{
+            startBoard = new int[][] {
+                {5,3,0,0,7,0,0,0,0},
+                {6,0,0,1,9,5,0,0,0},
+                {0,9,8,0,0,0,0,6,0},
+                {8,0,0,0,6,0,0,0,3},
+                {4,0,0,8,0,3,0,0,1},
+                {7,0,0,0,2,0,0,0,6},
+                {0,6,0,0,0,0,2,8,0},
+                {0,0,0,4,1,9,0,0,5},
+                {0,0,0,0,8,0,0,7,9}
+            };
+        }
 
         // SudokuSolver solver = new SudokuSolver(startBoard, 1000);
 
